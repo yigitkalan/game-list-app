9769e924e100f807acf0a71bf7b890f105772702
samet <sametykalan@gmail.com>
2022-04-12 15:59:37 +0300

    enhance structure of app

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -6,16 +6,16 @@ import com.example.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.motto1),
+            Affirmation(R.string.motto2),
+            Affirmation(R.string.motto3),
+            Affirmation(R.string.motto4),
+            Affirmation(R.string.motto5),
+            Affirmation(R.string.motto6),
+            Affirmation(R.string.motto7),
+            Affirmation(R.string.motto8),
+            Affirmation(R.string.motto9),
+            Affirmation(R.string.motto10)
         )
     }
 }
